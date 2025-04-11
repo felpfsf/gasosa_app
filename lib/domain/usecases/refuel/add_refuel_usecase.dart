377e8df2333903fc8f0a285bdfdb62248c41e5ff
Felipe Ferreira <feferreira.sf@gmail.com>
2025-04-10 23:23:41 -0300

    ✨ Refactor refuel use cases to implement interface segregation

--- lib/domain/usecases/refuel/add_refuel_usecase.dart
+++ lib/domain/usecases/refuel/add_refuel_usecase.dart
@@ -4,13 +4,18 @@ import 'package:gasosa_app/domain/entities/refuel.dart';
 import 'package:gasosa_app/domain/repositories/refuel_repository.dart';
 import 'package:injectable/injectable.dart';
 
-@injectable
-class AddRefuelUsecase {
+abstract class IAddRefuelUsecase {
+  Future<Either<Failure, Refuel>> call(Refuel refuel);
+}
+
+@Injectable(as: IAddRefuelUsecase)
+class AddRefuelUsecase implements IAddRefuelUsecase {
   final RefuelRepository _repository;
 
   AddRefuelUsecase({required RefuelRepository repository})
     : _repository = repository;
 
+  @override
   Future<Either<Failure, Refuel>> call(Refuel refuel) async {
     return await _repository.addRefuel(refuel);
   }
--- lib/domain/usecases/refuel/update_refuel_usecase.dart
+++ lib/domain/usecases/refuel/update_refuel_usecase.dart
@@ -4,13 +4,18 @@ import 'package:gasosa_app/domain/entities/refuel.dart';
 import 'package:gasosa_app/domain/repositories/refuel_repository.dart';
 import 'package:injectable/injectable.dart';
 
-@injectable
-class UpdateRefuelUsecase {
+abstract class IUpdateRefuelUsecase {
+  Future<Either<Failure, void>> call(Refuel refuel);
+}
+
+@Injectable(as: IUpdateRefuelUsecase)
+class UpdateRefuelUsecase implements IUpdateRefuelUsecase {
   final RefuelRepository _refuelRepository;
 
   UpdateRefuelUsecase({required RefuelRepository refuelRepository})
     : _refuelRepository = refuelRepository;
 
+  @override
   Future<Either<Failure, void>> call(Refuel refuel) async {
     return await _refuelRepository.updateRefuel(refuel);
   }
