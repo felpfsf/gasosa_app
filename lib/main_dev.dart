cfa2b83153862ed60cf7fde92885e01eef2e7a6d
Felipe Ferreira <feferreira.sf@gmail.com>
2025-04-24 02:21:11 -0300

    ✨ Update main function to initialize Firebase before running the app

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gasosa_app/app/app.dart';
 import 'package:gasosa_app/core/config/app_config.dart';
 import 'package:gasosa_app/core/config/env.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   AppConfig.setEnvironment(Env.development);
   runApp(GasosaApp());
 }
