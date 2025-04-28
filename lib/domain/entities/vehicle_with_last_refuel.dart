f7483a6ac1fe538b060a6c5e805d35a8d959d566
Felipe Ferreira <feferreira.sf@gmail.com>
2025-04-28 10:43:30 -0300

    ✨ Add VehicleWithLastRefuel class to represent vehicles with their last refuel date and formatted display logic.

---
+++ lib/domain/entities/vehicle_with_last_refuel.dart
@@ -0,0 +1,26 @@
+import 'package:gasosa_app/core/extensions/datetime_extensions.dart';
+import 'package:gasosa_app/domain/entities/vehicle.dart';
+
+class VehicleWithLastRefuel {
+  final Vehicle vehicle;
+  final DateTime? lastRefuelDate;
+
+  VehicleWithLastRefuel({required this.vehicle, this.lastRefuelDate});
+
+  String get id => vehicle.id;
+  String get name => vehicle.name;
+  String? get plate => vehicle.plate;
+  String get fuelType => vehicle.fuelType;
+  String get userId => vehicle.userId;
+  DateTime get createdAt => vehicle.createdAt;
+
+  bool get hasRfuel => lastRefuelDate != null;
+
+  String get formattedLastRefuelDate {
+    if (lastRefuelDate == null) {
+      return 'Nunca abastecido';
+    }
+
+    return lastRefuelDate.formattedFullDate();
+  }
+}
