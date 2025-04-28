6093f06c9254ae6d6da7447dd90ea190f758c616
Felipe Ferreira <feferreira.sf@gmail.com>
2025-04-28 10:43:46 -0300

    ✨ Enhance datetime formatting methods with additional comments for clarity.

--- lib/core/extensions/datetime_extensions.dart
+++ lib/core/extensions/datetime_extensions.dart
@@ -1,11 +1,13 @@
 import 'package:intl/intl.dart';
 
 extension DatetimeExtensions on DateTime? {
+  // format like 03/03/2025
   String formattedDate() {
     if (this == null) return '--/--/----';
     return DateFormat('dd/MM/yyyy').format(this!);
   }
 
+  // format like 03 de março de 2025
   String formattedFullDate() {
     if (this == null) return '';
     return DateFormat('d \'de\' MMMM \'de\' y', 'pt_BR' ).format(this!);
