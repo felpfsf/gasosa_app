4c918a39ef0c61a244b12ac4b8ee0ec2d7c97d14
Felipe Ferreira <feferreira.sf@gmail.com>
2025-04-28 10:43:51 -0300

    ✨ Add date formatting initialization to main functions in both development and production files.

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -5,10 +5,12 @@ import 'package:gasosa_app/core/config/app_config.dart';
 import 'package:gasosa_app/core/config/env.dart';
 import 'package:gasosa_app/core/di/injection.dart';
 import 'package:gasosa_app/firebase_options_dev.dart'; // todo change to prod later
+import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
+  await initializeDateFormatting('pt_BR');
   await Firebase.initializeApp();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
