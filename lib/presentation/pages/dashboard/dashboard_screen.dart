7769f6a2060d653520288b5f3318a16520aa195c
Felipe Ferreira <feferreira.sf@gmail.com>
2025-05-02 14:53:44 -0300

    ✨ Refactor DashboardScreen to enhance vehicle loading logic and improve error handling

--- lib/presentation/pages/dashboard/dashboard_screen.dart
+++ lib/presentation/pages/dashboard/dashboard_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gasosa_app/domain/entities/vehicle_with_last_refuel.dart';
 import 'package:gasosa_app/presentation/cubits/user/auth_cubit.dart';
 import 'package:gasosa_app/presentation/cubits/vehicle/vehicle_cubit.dart';
 import 'package:gasosa_app/presentation/cubits/vehicle/vehicle_state.dart';
 import 'package:gasosa_app/presentation/pages/dashboard/widgets/vehicle_list_widget.dart';
 import 'package:gasosa_app/presentation/widgets/custom_loader.dart';
+import 'package:gasosa_app/presentation/widgets/gasosa_appbar.dart';
 import 'package:gasosa_app/presentation/widgets/gasosa_empty_state_widget.dart';
 import 'package:gasosa_app/presentation/widgets/gasosa_error_widget.dart';
+import 'package:gasosa_app/presentation/widgets/messages.dart';
 import 'package:gasosa_app/theme/app_spacing.dart';
 import 'package:gasosa_app/theme/app_theme.dart';
 import 'package:gasosa_app/theme/app_typography.dart';
@@ -22,18 +25,13 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   void initState() {
-    _loadVehicles();
-    super.initState();
-  }
-
-  void _loadVehicles() {
     final user = context.read<AuthCubit>().state.maybeWhen(authenticated: (user) => user, orElse: () => null);
 
     if (user != null) {
       context.read<VehicleCubit>().fetchVehicles(user.id);
     }
 
-    // context.read<VehicleCubit>().fetchVehiclesMock();
+    super.initState();
   }
 
   @override
@@ -46,62 +44,77 @@ class _DashboardScreenState extends State<DashboardScreen> {
       await context.read<AuthCubit>().logout();
     }
 
-    AppBar appBar = AppBar(
-      title: RichText(
-        text: TextSpan(
-          text: 'Bem vindo, ',
-          style: AppTypography.titleSm,
-          children: [TextSpan(text: username, style: AppTypography.titleSm.copyWith(color: AppColors.primary))],
-        ),
-      ),
-      leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-      actions: [IconButton(onPressed: () => logout(), icon: const Icon(Icons.logout))],
-    );
-
     return Scaffold(
-      appBar: appBar,
-      body: BlocBuilder<VehicleCubit, VehicleState>(
+      appBar: GasosaAppbar(
+        title: 'Bem vindo, $username',
+        centerTitle: true,
+        showBackButton: true,
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+        actions: [IconButton(onPressed: () => logout(), icon: const Icon(Icons.logout))],
+      ),
+      body: BlocConsumer<VehicleCubit, VehicleState>(
+        listener: (context, state) {
+          state.whenOrNull(
+            error: (message) {
+              Messages.showError(context, 'Erro ao carregar os veículos: $message');
+            },
+          );
+        },
+        listenWhen: (_, __) => false, // não ouvimos eventos aqui
         builder: (context, state) {
-          return state.when(
-            initial: () => const SizedBox.shrink(),
-            loading:
-                () => CustomLoader<VehicleCubit, VehicleState>(
-                  selector: (state) => state.maybeWhen(loading: () => true, orElse: () => false),
-                  isOverlay: true,
-                  size: 48,
-                ),
-            error: (message) => GasosaErrorWidget(message: message),
-            success: (_) => const SizedBox.shrink(),
-            loaded: (vehicles) {
-              if (vehicles.isEmpty) {
-                return GaososaEmptyStateWidget(
-                  title: 'Nenhum veículo encontrado',
-                  message: 'Você ainda não possui nenhum veículo cadastrado.',
-                  actionLabel: 'Adicionar veículo',
+          return BlocBuilder<VehicleCubit, VehicleState>(
+            buildWhen: (previous, current) => true,
+            builder: (context, current) {
+              final isLoading = current.maybeWhen(loading: () => true, orElse: () => false);
+              final wasLoaded = state.maybeWhen(loaded: (_) => true, orElse: () => false);
+
+              if (isLoading && wasLoaded) {
+                final List<VehicleWithLastRefuel> vehicles = state.maybeWhen(loaded: (v) => v, orElse: () => []);
+
+                return Stack(
+                  children: [
+                    _buildVehicleList(vehicles),
+                    CustomLoader<VehicleCubit, VehicleState>(selector: (_) => true, isOverlay: true, size: 48),
+                  ],
                 );
               }
-              return SingleChildScrollView(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  spacing: AppSpacing.md,
-                  children: [
-                    Text('Seus veículos', style: AppTypography.titleMd),
-                    VehicleListWidget(vehicles: vehicles),
-                  ],
-                ),
+
+              return current.when(
+                initial: () => const SizedBox.shrink(),
+                loading:
+                    () => CustomLoader<VehicleCubit, VehicleState>(selector: (_) => true, isOverlay: true, size: 48),
+                error: (message) => GasosaErrorWidget(message: message),
+                success: (_) => const SizedBox.shrink(),
+                loaded: (vehicles) => _buildVehicleList(vehicles),
               );
             },
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.push('/vehicles/register');
-        },
+        onPressed: () => context.push('/vehicles/register'),
         tooltip: 'Adicionar veículo',
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
         child: Icon(Icons.add, color: AppColors.text),
+      ),
+    );
+  }
+
+  Widget _buildVehicleList(List<VehicleWithLastRefuel> vehicles) {
+    if (vehicles.isEmpty) {
+      return GaososaEmptyStateWidget(
+        title: 'Nenhum veículo encontrado',
+        message: 'Você ainda não possui nenhum veículo cadastrado.',
+        actionLabel: 'Adicionar veículo',
+      );
+    }
+
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        spacing: AppSpacing.md,
+        children: [Text('Seus veículos', style: AppTypography.titleMd), VehicleListWidget(vehicles: vehicles)],
       ),
     );
   }
