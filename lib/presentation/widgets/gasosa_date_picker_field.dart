916b8c782e0c83009a0b9d0b1be07be5984ae91e
Felipe Ferreira <feferreira.sf@gmail.com>
2025-05-09 02:20:11 -0300

    ✨ Refactor GasosaDatePickerField to include validator and improve date handling

--- lib/presentation/widgets/gasosa_date_picker_field.dart
+++ lib/presentation/widgets/gasosa_date_picker_field.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gasosa_app/theme/app_spacing.dart';
+import 'package:gasosa_app/theme/app_theme.dart';
 import 'package:gasosa_app/theme/app_typography.dart';
 import 'package:intl/intl.dart';
 
@@ -9,6 +10,7 @@ class GasosaDatePickerField extends StatelessWidget {
   final DateTime? firstDate;
   final DateTime? lastDate;
   final Function(DateTime) onDateSelected;
+  final String? Function(DateTime?)? validator;
 
   const GasosaDatePickerField({
     super.key,
@@ -17,51 +19,55 @@ class GasosaDatePickerField extends StatelessWidget {
     this.firstDate,
     this.lastDate,
     required this.onDateSelected,
+    this.validator,
   });
 
   @override
   Widget build(BuildContext context) {
-    final controller = TextEditingController(
-      text:
-          initialDate?.toString() != null
-              ? DateFormat('dd/MM/yyyy').format(initialDate!)
-              : '',
-    );
+    return FormField<DateTime>(
+      initialValue: initialDate,
+      validator: validator,
+      builder: (FormFieldState<DateTime> state) {
+        final formattedDate = state.value != null ? DateFormat('dd/MM/yyyy').format(state.value!) : '';
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(label),
-        AppSpacing.gap8,
-        GestureDetector(
-          onTap: () async {
-            final now = DateTime.now();
-            final pickedDate = await showDatePicker(
-              context: context,
-              initialDate: initialDate ?? now,
-              firstDate: firstDate ?? DateTime(now.year - 5),
-              lastDate: lastDate ?? DateTime(now.year + 5),
-              builder:
-                  (context, child) =>
-                      Theme(data: Theme.of(context), child: child!),
-            );
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: AppSpacing.sm,
+          children: [
+            Text(label, style: AppTypography.textSmBold),
+            GestureDetector(
+              onTap: () async {
+                final now = DateTime.now();
+                final pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: state.value ?? now,
+                  firstDate: firstDate ?? DateTime(now.year - 5),
+                  lastDate: lastDate ?? DateTime(now.year + 5),
+                  builder: (context, child) => Theme(data: Theme.of(context), child: child!),
+                );
 
-            if (pickedDate != null) {
-              onDateSelected(pickedDate);
-            }
-          },
-          child: AbsorbPointer(
-            child: TextField(
-              controller: controller,
-              style: AppTypography.textMdRegular,
-              decoration: InputDecoration(
-                hintText: 'dd/mm/yyyy',
-                suffixIcon: Icon(Icons.calendar_today),
+                if (pickedDate != null) {
+                  state.didChange(pickedDate);
+                  onDateSelected(pickedDate);
+                }
+              },
+              child: AbsorbPointer(
+                child: TextField(
+                  controller: TextEditingController(text: formattedDate),
+                  style: AppTypography.textMdRegular,
+                  decoration: InputDecoration(
+                    hintText: '01/01/2025',
+                    suffixIcon: Icon(Icons.calendar_today),
+                    hintStyle: AppTypography.textSmRegular.copyWith(color: AppColors.text.withValues(alpha: .6)),
+                    border: const OutlineInputBorder(),
+                    errorText: state.errorText,
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
