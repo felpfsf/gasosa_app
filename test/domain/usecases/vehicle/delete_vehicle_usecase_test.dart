cceff590c47f3e00fec937b8bd6ac089ba04e38c
Felipe Ferreira <feferreira.sf@gmail.com>
2025-05-12 12:45:22 -0300

    ✨ Refactor delete_vehicle_usecase_test to improve test clarity and ensure both vehicle and refuel deletions are verified.

--- test/domain/usecases/vehicle/delete_vehicle_usecase_test.dart
+++ test/domain/usecases/vehicle/delete_vehicle_usecase_test.dart
@@ -9,6 +9,7 @@ import '../../../mocks/mock_classes.dart';
 
 void main() {
   late MockVehicleRepository mockVehicleRepository;
+  late MockRefuelRepository mockRefuelRepository;
   late DeleteVehicleUsecase deleteVehicleUsecase;
 
   setUpAll(() {
@@ -17,34 +18,55 @@ void main() {
 
   setUp(() {
     mockVehicleRepository = MockVehicleRepository();
+    mockRefuelRepository = MockRefuelRepository();
+
     deleteVehicleUsecase = DeleteVehicleUsecase(
       vehicleRepository: mockVehicleRepository,
+      refuelRepository: mockRefuelRepository,
     );
   });
 
-  test('should return void when delete is successfull', () async {
+  test('should return void when both deletions are successfull', () async {
     final id = '1';
 
-    when(
-      () => mockVehicleRepository.deleteVehicle(id),
-    ).thenAnswer((_) async => Right(null));
+    when(() => mockRefuelRepository.deleteRefuelsByVehicleId(id)).thenAnswer((_) async => Right(null));
+
+    when(() => mockVehicleRepository.deleteVehicle(id)).thenAnswer((_) async => Right(null));
 
     final result = await deleteVehicleUsecase(id);
 
-    expect(result, null);
+    expect(result, Right(null));
+    verify(() => mockRefuelRepository.deleteRefuelsByVehicleId(id)).called(1);
     verify(() => mockVehicleRepository.deleteVehicle(id)).called(1);
   });
 
-  test('should return failure when delete is failed', () async {
+  test('should return failure when deleting refuels is failed', () async {
     final id = '1';
 
     when(
-      () => mockVehicleRepository.deleteVehicle(id),
-    ).thenAnswer((_) async => Left(DatabaseFailure('error')));
+      () => mockRefuelRepository.deleteRefuelsByVehicleId(id),
+    ).thenAnswer((_) async => Left(DatabaseFailure('Erro ao deletar reabastecimentos do veículo')));
 
     final result = await deleteVehicleUsecase(id);
 
     expect(result, isA<Left<Failure, void>>());
+    verify(() => mockRefuelRepository.deleteRefuelsByVehicleId(id)).called(1);
+    verifyNever(() => mockVehicleRepository.deleteVehicle(id));
+  });
+
+  test('should return failure when deleting vehicle is failed', () async {
+    final id = '1';
+
+    when(() => mockRefuelRepository.deleteRefuelsByVehicleId(id)).thenAnswer((_) async => Right(null));
+
+    when(
+      () => mockVehicleRepository.deleteVehicle(id),
+    ).thenAnswer((_) async => Left(DatabaseFailure('Erro ao deletar veículo')));
+
+    final result = await deleteVehicleUsecase(id);
+
+    expect(result, isA<Left<Failure, void>>());
+    verify(() => mockRefuelRepository.deleteRefuelsByVehicleId(id)).called(1);
     verify(() => mockVehicleRepository.deleteVehicle(id)).called(1);
   });
 }
