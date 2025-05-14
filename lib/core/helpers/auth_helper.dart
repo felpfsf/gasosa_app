cf952f29d4062ccded3defa4c92a9584d4cfc4c5
Felipe Ferreira <feferreira.sf@gmail.com>
2025-05-13 21:52:47 -0300

    ✨ Refactor AuthHelper to ensure proper user authentication handling

--- lib/core/helpers/auth_helper.dart
+++ lib/core/helpers/auth_helper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gasosa_app/domain/entities/user.dart';
 import 'package:gasosa_app/presentation/cubits/user/auth_cubit.dart';
 
 class AuthHelper {
@@ -8,4 +9,13 @@ class AuthHelper {
 
     return state.maybeWhen(authenticated: (user) => user.id, orElse: () => null);
   }
+
+  static User getAuthenticatedUser(BuildContext context) {
+  final state = context.read<AuthCubit>().state;
+
+  return state.maybeWhen(
+    authenticated: (user) => user,
+    orElse: () => throw StateError('User is not authenticated'),
+  );
+}
 }
