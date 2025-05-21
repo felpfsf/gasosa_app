850c8e9a71bffb2285e2c67406c064a4fda71e25
Felipe Ferreira <feferreira.sf@gmail.com>
2025-05-20 22:34:23 -0300

    ✨ Refactor formatters: update currency formatting and streamline parsing functions

--- lib/core/helpers/formatters.dart
+++ lib/core/helpers/formatters.dart
@@ -4,14 +4,12 @@ import 'package:flutter_multi_formatter/formatters/money_input_enums.dart';
 import 'package:intl/intl.dart';
 
 final litersInputFormatter = MaskedInputFormatter('##.###', allowedCharMatcher: RegExp(r'[0-9]'));
-
 final decimalInputFormatter = MaskedInputFormatter('###.###', allowedCharMatcher: RegExp(r'[0-9]'));
-
 final integerInputFormatter = MaskedInputFormatter('######', allowedCharMatcher: RegExp(r'[0-9]'));
 
 final currencyInputFormatter = CurrencyInputFormatter(
   leadingSymbol: 'R\$',
-  thousandSeparator: ThousandSeparator.Comma,
+  thousandSeparator: ThousandSeparator.Period,
   useSymbolPadding: true,
 );
 
@@ -24,41 +22,36 @@ double parseMaskedInteger(String value) {
 double parseMaskedCurrency(String value) {
   final cleaned = value
       .replaceAll('R\$', '')
-      // .replaceAll('.', '')
-      .replaceAll(',', '')
+      .replaceAll('.', '') 
+      .replaceAll(',', '.') 
       .replaceAll(RegExp(r'[^\d.]'), '');
 
   return double.tryParse(cleaned) ?? 0.0;
 }
 
 double parseMaskedLiters(String value) {
-  final cleaned = value.replaceAll(RegExp(r'[^\d.]'), '');
+  final cleaned = value
+      .replaceAll(RegExp(r'[^\d.]'), '');
 
   return double.tryParse(cleaned) ?? 0.000;
 }
 
 String formatMaskedCurrency(double value) {
-  final currency = NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$').format(value);
-  return currency;
+  return NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$').format(value);
 }
 
 String formatMaskedLiters(double value) {
-  final liters = NumberFormat('#0.###', 'pt_BR').format(value);
-  return liters;
+  final formatted = NumberFormat('#0.###', 'pt_BR').format(value);
+  return formatted.replaceAll(',', '.');
 }
 
 String formatDistance(double? distance) {
   if (distance == null) return '-';
-  if (distance < 1.0) {
-    final meters = (distance * 1000).round();
-    return '$meters m';
-  }
-
+  if (distance < 1.0) return '${(distance * 1000).round()} m';
   return '${distance.toStringAsFixed(0)} km';
 }
 
 String formatConsumption(double? consumption, String unit) {
   if (consumption == null || consumption <= 0) return '-';
-
   return '${consumption.toStringAsFixed(1)} km/$unit';
 }
